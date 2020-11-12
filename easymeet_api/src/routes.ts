2534206c816719b0cd77674ade9d11ac53bfd038
pedrorossato <phrossato@inf.ufsm.br>
2020-11-12 10:57:49 -0300

    listagem de eventos no backend, porém bad request ao tentar criar evento

--- easymeet_api/src/routes.ts
+++ easymeet_api/src/routes.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
-
+import { Request, Response, Router } from 'express';
+import EventsController from "./controllers/EventsController";
 const routes = Router();
 
-routes.get('/', );
-
+routes.get('/events', EventsController.index);
+routes.post('/events',EventsController.create);
 export default routes;
